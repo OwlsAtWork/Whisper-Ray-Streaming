490eb474db296e4d121743063c1129bdb699a3fe
rohit <rrohitjee2018@gmail.com>
2025-04-01 00:09:46 +0530

    updated envs

--- src/voice_stream_ai_server.py
+++ src/voice_stream_ai_server.py
@@ -1,24 +1,23 @@
+from .vad.vad_factory import VADFactory
+from .vad.silero_vad import SileroVAD
+from .client import Client
+from .asr.faster_whisper_asr import FasterWhisperASR
+from .asr.asr_factory import ASRFactory
+from ray.serve.handle import DeploymentHandle
+from ray import serve
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+import uuid
+import logging
+import json
 import faulthandler
 faulthandler.enable()
 
-import json
-import logging
-import uuid
-
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from ray import serve
-from ray.serve.handle import DeploymentHandle
-
-from .asr.asr_factory import ASRFactory
-from .asr.faster_whisper_asr import FasterWhisperASR
-from .client import Client
-from .vad.silero_vad import SileroVAD
-from .vad.vad_factory import VADFactory
 
 logger = logging.getLogger("ray.serve")
 logger.setLevel(logging.INFO)
 
 fastapi_app = FastAPI()
+
 
 @serve.deployment
 @serve.ingress(fastapi_app)
@@ -43,10 +42,10 @@ class TranscriptionServer:
         self.debug_output = {
             "transcriptions_timestamp": {},
             "silence_detection_timestamp": {},
-            "silence_threshold_seconds": 0.5,
+            "silence_threshold_seconds": 0.6,
             "min_speech_seconds": 0.5,
             "max_buffer_seconds": 3000.0,
-            "buffer_context_seconds_for_vad": 2.0,
+            "buffer_context_seconds_for_vad": 1.0,
             "client_id": None,
             "original_buffer_size": None,
             "audio_shape": None,
@@ -54,7 +53,6 @@ class TranscriptionServer:
             "audio_duration": None,
             "total_processing_time": None,
         }
-        
 
     async def handle_audio(self, client: Client, websocket: WebSocket):
         """
@@ -68,7 +66,8 @@ class TranscriptionServer:
             elif "text" in message.keys():
                 config = json.loads(message['text'])
                 if config.get('type') == 'config':
-                    logger.debug(f"Received config update from client {client.client_id}")
+                    logger.debug(
+                        f"Received config update from client {client.client_id}")
                     client.update_config(config['data'])
                     continue
             elif message["type"] == "websocket.disconnect":
@@ -76,9 +75,11 @@ class TranscriptionServer:
                 raise WebSocketDisconnect
             else:
                 keys_list = list(message.keys())
-                logger.error(f"{type(message)} is not a valid message type. Type is {message['type']}; keys: {json.dumps(keys_list)}")
+                logger.error(
+                    f"{type(message)} is not a valid message type. Type is {message['type']}; keys: {json.dumps(keys_list)}")
 
-            client.process_audio(websocket, self.vad_handle, self.asr_handle, self.debug_output)
+            client.process_audio(websocket, self.vad_handle,
+                                 self.asr_handle, self.debug_output)
 
     @fastapi_app.websocket("/")
     async def handle_websocket(self, websocket: WebSocket):
@@ -100,7 +101,9 @@ class TranscriptionServer:
             logger.info(f"Removing client {client_id} from connected clients")
             del self.connected_clients[client_id]
 
+
 logger.info("Starting TranscriptionServer deployment")
-entrypoint = TranscriptionServer.bind(FasterWhisperASR.bind(), SileroVAD.bind())
+entrypoint = TranscriptionServer.bind(
+    FasterWhisperASR.bind(), SileroVAD.bind())
 serve.run(entrypoint)
 logger.info("TranscriptionServer is running")
